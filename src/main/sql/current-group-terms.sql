653d00ae4306bae0937ca7ece493bbccc02a53f8
Dave Briccetti <daveb@davebsoft.com>
2018-08-27 11:24:24 -0700

    Update some info for the current year

--- src/main/sql/current-group-terms.sql
+++ src/main/sql/current-group-terms.sql
@@ -1,1 +1,1 @@
-select * from "GroupTerm" gt left join music_group mg on gt."groupId" = mg.id where term = 2016 order by period;
+select * from "GroupTerm" gt left join music_group mg on gt."groupId" = mg.id where term = 2017 order by period;
